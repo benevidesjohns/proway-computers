fa38c45d4bda339e2f8231dbefadd352fdf58a20
benevidesjohns <benevidesjohns@gmail.com>
2024-03-19 08:43:49 -0300

    feat: page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const ROUTES: Routes = [
   {
@@ -10,6 +11,11 @@ const ROUTES: Routes = [
     path: '',
     redirectTo: "produtos",
     pathMatch: "full"
+  },
+  //
+  {
+    path: "**",
+    component: PageNotFoundComponent
   }
 ];
 
